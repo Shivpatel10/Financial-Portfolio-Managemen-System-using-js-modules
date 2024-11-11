a454e41b841f3c9ee120c873b567db6f61f06aed
Shiv Patel <shivpatel0204@gmail.com>
2024-11-10 21:05:03 -0500

    Create the Asset Module

--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,5 @@
 // Task 2: Create Portfolio Module
-import { assetList } from "./asset"; //Importing named exports from portfolio.js
+import { assetList } from "./asset.js"; //Importing named exports from portfolio.js
 
 export function calculatePortfolio(assetList) { //function to sum the total value of portfolio
     return assetList.reduce((totalValue, asset) => {
